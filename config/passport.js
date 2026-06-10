f127ad82de22f628de43937f908d65fe8045d876
Majidullask04 <158665684+Majidullask04@users.noreply.github.com>
2026-06-10 18:09:28 +0530

    disable twitter auth

--- config/passport.js
+++ config/passport.js
@@ -9,7 +9,7 @@ const User = mongoose.model('User');
 
 const local = require('./passport/local');
 const google = require('./passport/google');
-const twitter = require('./passport/twitter');
+// const twitter = require('./passport/twitter');
 const linkedin = require('./passport/linkedin');
 const github = require('./passport/github');
 
@@ -27,7 +27,7 @@ module.exports = function(passport) {
   // use these strategies
   passport.use(local);
   passport.use(google);
-  passport.use(twitter);
+  // passport.use(twitter);
   passport.use(linkedin);
   passport.use(github);
 };
